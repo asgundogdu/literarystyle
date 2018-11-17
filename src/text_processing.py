8a334cfd81f5533f309e43ec389f0f1517432cae
fhooton <yourfriendforrest@gmail.com>
2018-11-17 15:53:46 -0500

    At least includes output of text-processing

--- src/text_processing.py
+++ src/text_processing.py
@@ -22,6 +22,8 @@ class data():
 		# Whether or not we want to take a subset of the dataframe
 		self.subset = True
 
+	# Will process all the text in spacy if we want to. Lot's of different nlp options. Note, rate is about 5000
+	# articles per hour on 16gb RAM
 	def __spacy__(self):
 
 		if self.subset:
@@ -34,4 +36,17 @@ class data():
 
 			if not idx % 5000:
 				print(idx, "rows in", (time()-start)/60, 'min')
+
+	# The primary function that builds the processed data
+	# Once run, the data that can be output is self.pdata
+	# Could also just return pdata later on, if that's a better design choice
+	def get_processed_data(self):
+
+		self.pdata = self.dataframe.content.tolist()
+
+		start = time()
+
+		self.pdata = [c.split() for c in self.pdata]
+
+		print('Simple splitting done in:', (time()-start)/60, 'min')
 
