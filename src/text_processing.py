3b4c6ecfff6058bdbf3f59c80cae022e12ee0fac
fhooton <yourfriendforrest@gmail.com>
2018-11-25 20:26:38 -0500

    Added Stemming and Lemmatization

--- src/text_processing.py
+++ src/text_processing.py
@@ -13,6 +13,8 @@ import pickle
 import string
 import re
 from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
 
 from .config import *
@@ -31,6 +33,8 @@ class data():
 		#self.nlp = en_core_web_sm.load()
 
 		self.stopwords = stopwords.words('english')#{s : True for s in stopwords.words('english')}
+		self.stemmer = SnowballStemmer('english')
+		self.lmtzr = WordNetLemmatizer()
 
 	# Will process all the text in spacy if we want to. Lot's of different nlp options. Note, rate is about 5000
 	# articles per hour on 16gb RAM
@@ -47,7 +51,7 @@ class data():
 			if not idx % 5000:
 				print(idx, "rows in", (time()-start)/60, 'min')
 
-	def __vader_polarity__(self):
+	def vader_polarity(self):
 		self.vader_analyzer = SentimentIntensityAnalyzer()
 		self.vader_list = []
 
@@ -78,6 +82,16 @@ class data():
 	def filter_by_sentiment(self):
 		self.pdata = [[i['word'] for i in j if abs(i['compound']) < config_sent_threshold] for j in self.vader_list]
 
+	def stem_vocab(self, w_lemma=True):
+		start = time()
+
+		if w_lemma:
+			self.pdata = [[self.lmtzr.lemmatize(self.stemmer.stem(word)) for word in j] for j in self.pdata]
+			print('Stemming and lemmatization done in', (time()-start) / 60, 'min')
+		else:
+			self.pdata = [[self.stemmer.stem(word) for word in j] for j in self.pdata]
+			print('Stemming done in', (time()-start) / 60, 'min')
+
 	# The primary function that builds the processed data
 	# Once run, the data that can be output is self.pdata
 	# Could also just return pdata later on, if that's a better design choice
@@ -91,5 +105,7 @@ class data():
 			start = time()
 
 			self.pdata = [[i for i in re.sub(r'[^\w\s]','',c.lower()).split() if i not in self.stopwords] for c in self.pdata]
+			# Numerical processing from homework assignment 4
+			self.pdata = [['NUM' if re.match('[0-9]+', word) is not None else word for word in c ] for c in self.pdata]
 			print('Simple splitting done in:', (time()-start)/60, 'min')
 
